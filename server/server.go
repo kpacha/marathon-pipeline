df7e2fbcaa8b730789d0cd4453d588999cd70946
Kpacha <kpacha666@gmail.com>
2015-12-20 17:18:11 +0100

    REST api added

---
+++ server/server.go
@@ -0,0 +1,70 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kpacha/marathon-pipeline/worker"
+)
+
+type Job struct {
+	ID     string
+	Name   string
+	Filter *worker.FilterConstraint
+	Params map[string]string
+}
+
+type GinServer struct {
+	Port   int
+	engine *gin.Engine
+	jobs   map[string]Job
+}
+
+func Default(port int) GinServer {
+	return GinServer{port, gin.Default(), map[string]Job{}}
+}
+
+func (s *GinServer) Run() {
+	s.engine.GET("", func(c *gin.Context) {
+		c.JSON(http.StatusOK, s.jobs)
+	})
+	s.engine.POST("", func(c *gin.Context) {
+		var job Job
+		err := c.Bind(&job)
+		if err != nil {
+			c.JSON(
+				http.StatusBadRequest,
+				gin.H{"status": fmt.Sprintf("Error processing the body of the request: %s", err.Error())})
+			return
+		}
+		if job.ID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "Error: Job Id must be set!"})
+			return
+		}
+		if _, ok := s.jobs[job.ID]; ok {
+			c.JSON(http.StatusBadRequest, gin.H{"status": fmt.Sprintf("Job already exists: %s", job.ID)})
+			return
+		}
+		s.jobs[job.ID] = job
+		c.JSON(http.StatusOK, s.jobs[job.ID])
+	})
+	s.engine.GET("/:jobId", func(c *gin.Context) {
+		jobId := c.Params.ByName("jobId")
+		if j, ok := s.jobs[jobId]; ok {
+			c.JSON(http.StatusOK, j)
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{"status": fmt.Sprintf("Job not found: %s", jobId)})
+		}
+	})
+	s.engine.DELETE("/:jobId", func(c *gin.Context) {
+		jobId := c.Params.ByName("jobId")
+		if _, ok := s.jobs[jobId]; ok {
+			delete(s.jobs, jobId)
+			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{"status": fmt.Sprintf("Job not found: %s", jobId)})
+		}
+	})
+	s.engine.Run(fmt.Sprintf(":%d", s.Port))
+}
---
+++ server/server_test.go
@@ -0,0 +1,120 @@
+package server
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServer(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+	testGinServer := GinServer{9876, gin.Default(), map[string]Job{"supu": Job{Name: "Name to display", ID: "supu"}}}
+	go func() {
+		testGinServer.Run()
+	}()
+	time.Sleep(5 * time.Millisecond)
+
+	testServerGetMethods(t, testGinServer.engine)
+	testServerPostMethods(t, testGinServer.engine)
+	testServerDeleteMethods(t, testGinServer.engine)
+}
+
+func testServerGetMethods(t *testing.T, gs *gin.Engine) {
+	getCases := []struct {
+		url    string
+		code   int
+		result string
+	}{
+		{"/", 200, "{\"supu\":{\"ID\":\"supu\",\"Name\":\"Name to display\",\"Filter\":null,\"Params\":null}}\n"},
+		{"/supu", 200, "{\"ID\":\"supu\",\"Name\":\"Name to display\",\"Filter\":null,\"Params\":null}\n"},
+		{"/unknown", 404, "{\"status\":\"Job not found: unknown\"}\n"},
+	}
+
+	for _, c := range getCases {
+		w := performGetRequest(gs, c.url)
+		assert.Equal(t, c.code, w.Code)
+		assert.Equal(t, c.result, w.Body.String())
+	}
+}
+
+func testServerPostMethods(t *testing.T, gs *gin.Engine) {
+	postCases := []struct {
+		payload string
+		code    int
+		result  string
+	}{
+		{
+			"{\"id\":\"new\",\"name\":\"new\",\"filter\":null,\"params\":null}",
+			200,
+			"{\"ID\":\"new\",\"Name\":\"new\",\"Filter\":null,\"Params\":null}\n",
+		},
+		{
+			"{\"id\":\"supu\",\"name\":\"\",\"filter\":null,\"params\":null}",
+			400,
+			"{\"status\":\"Job already exists: supu\"}\n",
+		},
+		{
+			"{",
+			400,
+			"{\"status\":\"Error processing the body of the request: unexpected EOF\"}\n",
+		},
+		{
+			"{\"Name\":\"Name to display\"}",
+			400,
+			"{\"status\":\"Error: Job Id must be set!\"}\n",
+		},
+	}
+
+	for _, c := range postCases {
+		w := performRequest(gs, "POST", "/", c.payload)
+		assert.Equal(t, c.code, w.Code)
+		assert.Equal(t, c.result, w.Body.String())
+	}
+}
+
+func testServerDeleteMethods(t *testing.T, gs *gin.Engine) {
+	w := performDeleteRequest(gs, "/new")
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"status\":\"ok\"}\n", w.Body.String())
+
+	w = performDeleteRequest(gs, "/new")
+	assert.Equal(t, 404, w.Code)
+	assert.Equal(t, "{\"status\":\"Job not found: new\"}\n", w.Body.String())
+}
+
+func performRequest(r http.Handler, method, path, payload string) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(method, path, bytes.NewBuffer([]byte(payload)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	return recordServerRequest(r, req)
+}
+
+func performGetRequest(r http.Handler, path string) *httptest.ResponseRecorder {
+	return performEmptyRequest(r, "GET", path)
+}
+
+func performDeleteRequest(r http.Handler, path string) *httptest.ResponseRecorder {
+	return performEmptyRequest(r, "DELETE", path)
+}
+
+func performEmptyRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(method, path, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return recordServerRequest(r, req)
+}
+
+func recordServerRequest(r http.Handler, req *http.Request) *httptest.ResponseRecorder {
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
