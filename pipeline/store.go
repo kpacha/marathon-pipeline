a67bd2b63955712c9701c70e98d11bbe634befc0
Kpacha <kpacha666@gmail.com>
2015-12-25 01:07:10 +0100

    subscriptions to the task store snapshot stream added

--- pipeline/store.go
+++ pipeline/store.go
@@ -5,10 +5,17 @@ type TaskStore interface {
 	Get(k string) (Task, bool, error)
 	Set(t Task) error
 	Delete(k string) error
+	Subscribe() (TaskStoreSubscription, error)
+}
+
+type TaskStoreSubscription struct {
+	SnapshotStream chan map[string]Task
+	ErrorStream    chan error
 }
 
 type MemoryTaskStore struct {
-	Store map[string]Task
+	Store         map[string]Task
+	Subscriptions *[]TaskStoreSubscription
 }
 
 func (m MemoryTaskStore) GetAll() (map[string]Task, error) {
@@ -22,10 +29,34 @@ func (m MemoryTaskStore) Get(k string) (Task, bool, error) {
 
 func (m MemoryTaskStore) Set(t Task) error {
 	m.Store[t.ID] = t
+	m.notifySubscriptors()
 	return nil
 }
 
 func (m MemoryTaskStore) Delete(k string) error {
 	delete(m.Store, k)
+	m.notifySubscriptors()
 	return nil
+}
+
+func (m MemoryTaskStore) Subscribe() (TaskStoreSubscription, error) {
+	susbscription := TaskStoreSubscription{
+		make(chan map[string]Task, 1000),
+		make(chan error, 1000),
+	}
+	*m.Subscriptions = append(*m.Subscriptions, susbscription)
+	return susbscription, nil
+}
+
+func (m MemoryTaskStore) notifySubscriptors() {
+	if m.Subscriptions == nil {
+		return
+	}
+	for _, subscriptor := range *m.Subscriptions {
+		go m.notify(subscriptor.SnapshotStream, m.Store)
+	}
+}
+
+func (m MemoryTaskStore) notify(channel chan map[string]Task, snapshot map[string]Task) {
+	channel <- snapshot
 }
---
+++ pipeline/store_test.go
@@ -0,0 +1,100 @@
+package pipeline
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemoryTaskStore_GetAll(t *testing.T) {
+	initialSnapshot := map[string]Task{"supu": Task{Name: "Name to display", ID: "supu"}}
+	store, _, _ := newMemoryTaskStore(initialSnapshot)
+
+	snapshot, err := store.GetAll()
+	assert.Equal(t, initialSnapshot, snapshot)
+	assert.Equal(t, nil, err)
+}
+
+func TestMemoryTaskStore_Get(t *testing.T) {
+	taskId := "supu"
+	initialSnapshot := map[string]Task{taskId: Task{Name: "Name to display", ID: taskId}}
+	store, _, _ := newMemoryTaskStore(initialSnapshot)
+
+	task, ok, err := store.Get(taskId)
+	assert.Equal(t, initialSnapshot[taskId], task)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, nil, err)
+
+	task, ok, err = store.Get("unknown")
+	assert.Equal(t, Task{}, task)
+	assert.Equal(t, false, ok)
+	assert.Equal(t, nil, err)
+}
+
+func TestMemoryTaskStore_Set(t *testing.T) {
+	initialSnapshot := map[string]Task{}
+	store, _, err := newMemoryTaskStore(initialSnapshot)
+	store, subscription, err := newMemoryTaskStore(initialSnapshot)
+	assert.Equal(t, nil, err)
+
+	testTask := Task{Name: "Name to display", ID: "supu"}
+	expectedSnapshot := map[string]Task{testTask.ID: testTask}
+
+	task, ok, err := store.Get(testTask.ID)
+	assert.Equal(t, Task{}, task)
+	assert.Equal(t, false, ok)
+	assert.Equal(t, nil, err)
+
+	err = store.Set(testTask)
+	assert.Equal(t, nil, err)
+
+	select {
+	case snapshot := <-subscription.SnapshotStream:
+		assert.Equal(t, expectedSnapshot, snapshot)
+	case <-time.After(5 * time.Millisecond):
+		t.Error("timeout waiting for the snapshot after a set")
+	}
+
+	task, ok, err = store.Get(testTask.ID)
+	assert.Equal(t, testTask, task)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, nil, err)
+}
+
+func TestMemoryTaskStore_Delete(t *testing.T) {
+	taskId := "supu"
+	initialSnapshot := map[string]Task{taskId: Task{Name: "Name to display", ID: taskId}}
+	store, subscription, err := newMemoryTaskStore(initialSnapshot)
+	assert.Equal(t, nil, err)
+
+	task, ok, err := store.Get(taskId)
+	assert.Equal(t, initialSnapshot[taskId], task)
+	assert.Equal(t, true, ok)
+	assert.Equal(t, nil, err)
+
+	err = store.Delete(taskId)
+	assert.Equal(t, nil, err)
+
+	select {
+	case snapshot := <-subscription.SnapshotStream:
+		assert.Equal(t, initialSnapshot, snapshot)
+		assert.Equal(t, map[string]Task{}, snapshot)
+	case <-time.After(5 * time.Millisecond):
+		t.Error("timeout waiting for the snapshot after a delete")
+	}
+
+	task, ok, err = store.Get(taskId)
+	assert.Equal(t, Task{}, task)
+	assert.Equal(t, false, ok)
+	assert.Equal(t, nil, err)
+}
+
+func newMemoryTaskStore(snapshot map[string]Task) (MemoryTaskStore, TaskStoreSubscription, error) {
+	store := MemoryTaskStore{
+		Store:         snapshot,
+		Subscriptions: &[]TaskStoreSubscription{},
+	}
+	subscription, err := store.Subscribe()
+	return store, subscription, err
+}
